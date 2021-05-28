e38fbeb55c8ce5c087e27cd0b9db04d98e4e3445
GyanP <gyanig72@gmail.com>
2021-05-28 22:08:38 +0530

    changes in code for heroku deploy

--- server.js
+++ server.js
@@ -18,6 +18,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')));
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+
 app.use('/', require('./routers/router'));
 
 app.listen(PORT, () => {
