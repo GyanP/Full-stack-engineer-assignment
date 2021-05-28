15bdc435dc2e54042744a0c0d49cfe5bc77d183d
GyanP <gyanig72@gmail.com>
2021-05-28 18:24:20 +0530

    update server.js for build

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-require("dotenv").config();
+require('dotenv').config();
 
-const db = require("./db/db");
+const db = require('./db/db');
 
 const app = express();
 
@@ -18,7 +18,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.use("/", require("./routers/router"));
+const buildPath = path.join(__dirname, 'build');
+app.use(express.static(buildPath));
+
+app.use('/', require('./routers/router'));
 
 app.listen(PORT, () => {
   db.sync({ alert: true });
