5bdda2966e64813e02f79bdf017bc874dd88e1f1
GyanP <gyanig72@gmail.com>
2021-06-01 17:23:07 +0530

    db code comment

--- db/db.js
+++ db/db.js
@@ -1,22 +1,22 @@
-require('dotenv').config();
+// require('dotenv').config();
 
-const Sequelize = require('sequelize');
+// const Sequelize = require('sequelize');
 
-//Establishing MySQL database connection using Sequelize
-const db = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    pool: {
-      max: 10,
-      min: 0,
-      acquire: 30000,
-      idle: 10000,
-    },
-  }
-);
+// //Establishing MySQL database connection using Sequelize
+// const db = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASS,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: 'postgres',
+//     pool: {
+//       max: 10,
+//       min: 0,
+//       acquire: 30000,
+//       idle: 10000,
+//     },
+//   }
+// );
 
-module.exports = db;
+// module.exports = db;
