3049d1987ec9d055d2263f829c7df4f8a3ef3f07
GyanP <gyanig72@gmail.com>
2021-06-01 14:53:23 +0530

    server file changes

--- server.js
+++ server.js
@@ -21,13 +21,13 @@ app.use(bodyParser.json());
 // Serve static files from the React frontend app
 app.use(express.static(path.join(__dirname, 'client/build')));
 // Anything that doesn't match the above, send back index.html
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
 app.use('/', require('./routers/router'));
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   db.sync({ alert: true });
   console.log(`server is runing on Port ${PORT}`);
 });
