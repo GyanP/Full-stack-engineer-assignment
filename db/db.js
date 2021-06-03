6dd85b0ce88184e427bed244f723746286bb1c11
GyanP <gyanig72@gmail.com>
2021-06-03 19:28:48 +0530

    db setup

--- db/db.js
+++ db/db.js
@@ -1,17 +1,19 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const Sequelize = require("sequelize");
+const Sequelize = require('sequelize');
 
 //Establishing MySQL database connection using Sequelize
+
 const db = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
+  'd4pvnf0ub7fqkj',
+  'asxaxoaoqxdzrk',
+  'd7de2f29f37a1bacfd74256d7392a7967fbd217339a0b985d19ad8bef42a9c5a',
   {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
+    host: 'ec2-3-89-0-52.compute-1.amazonaws.com',
+    dialect: 'postgres',
+    operatorsAliases: false,
     pool: {
-      max: 10,
+      max: 5,
       min: 0,
       acquire: 30000,
       idle: 10000,
