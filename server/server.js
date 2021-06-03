90662fd642da0c1c6cf84ce0fb4c432ac3c059ed
GyanP <gyanig72@gmail.com>
2021-06-03 16:45:12 +0530

    code push

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 require('dotenv').config();
+const http = require('http');
 
 const db = require('./db/db');
 
@@ -20,7 +21,16 @@ app.use(bodyParser.json());
 
 app.use('/', require('./routers/router'));
 
-app.listen(PORT, () => {
+// Setup static directory to serve
+app.use(express.static(path.resolve('client', 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve('client', 'build', 'index.html'));
+});
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
   db.sync({ alert: true });
   console.log(`server is runing on Port ${PORT}`);
 });
